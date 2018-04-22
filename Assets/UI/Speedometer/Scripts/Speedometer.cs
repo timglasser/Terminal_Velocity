828a4cedf75af8027da491265f95877c58487bab
JooPJoo <38052366+JooPJoo@users.noreply.github.com>
2018-04-22 08:42:03 -0700

    New Speedometer
    Got the new speedometer up but it doesn't work

--- Assets/UI/Speedometer/Scripts/Speedometer.cs
+++ Assets/UI/Speedometer/Scripts/Speedometer.cs
@@ -5,7 +5,7 @@ using System.Collections;
 
 public class Speedometer : MonoBehaviour {
 
-	public GameObject vehicle;
+	public Rigidbody vehicle;
 	public Text speedometer;
 	private float MPH;
 
@@ -16,7 +16,7 @@ public class Speedometer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		MPH = vehicle.GetComponent<Rigidbody>().velocity.magnitude * 2.237f;
+		MPH = vehicle.velocity.magnitude * 2.237f;
 		speedometer.text = "Name: RYDER";
 
 		transform.Find("SpeedometerNeedle").localEulerAngles = new Vector3(0f, 0f, Mathf.Round(-MPH * 100f / 100f) );
