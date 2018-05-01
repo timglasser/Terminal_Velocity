cd38dcf91ee0cfd01f44dbfce3c3e95dbef8347f
BPatte <bhp5000@yahoo.com>
2018-05-01 16:22:28 -0400

    Rain/Tunnel Trigger
    Added and tested working rain trigger to turn on/off rain when entering/exiting the tunnel.

---
+++ Assets/EntryTrigger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EntryTrigger : MonoBehaviour {
+
+    
+    //private bool showPopUp = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log(col.gameObject.tag);
+        if (col.gameObject.tag == "Tunnel")
+        {
+            transform.GetChild(0).gameObject.SetActive(!transform.GetChild(0).gameObject.activeSelf);
+            transform.GetChild(1).gameObject.SetActive(!transform.GetChild(1).gameObject.activeSelf);
+        }
+    }
+
+    void OnTriggerEnter(Collider obj)
+    {
+        Debug.Log(obj.tag);
+        if (obj.gameObject.tag == "Tunnel")
+        {
+            transform.GetChild(0).gameObject.SetActive(!transform.GetChild(0).gameObject.activeSelf);
+            transform.GetChild(1).gameObject.SetActive(!transform.GetChild(1).gameObject.activeSelf);
+        }
+    }
+}
