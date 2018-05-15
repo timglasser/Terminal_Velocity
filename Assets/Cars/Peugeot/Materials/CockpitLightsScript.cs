3cca925547e224332b0b6b9213618784dbeebd3a
Nathaniel Jaime <nejaime97@gmail.com>
2018-05-15 14:11:09 -0400

    mirror for car works
    added mirror functionality

---
+++ Assets/Cars/Peugeot/Materials/CockpitLightsScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CockpitLightsScript : MonoBehaviour
+{
+    public List<Light> Lights;
+
+    void OnPreRender()
+    {
+        foreach (Light light in Lights)
+        {
+            light.enabled = false;
+        }
+    }
+
+    void OnPostRender()
+    {
+        foreach (Light light in Lights)
+        {
+            light.enabled = true;
+        }
+    }
+}
