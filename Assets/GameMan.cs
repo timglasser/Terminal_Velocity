f1c09e525f2c3287ba0a234d0f13ca993a3ca7fe
PanattaE <38263469+PanattaE@users.noreply.github.com>
2018-05-15 22:59:33 -0400

    Camera Manager, Starting Game Manager, Setting Up Atrract Screen
    Adding Manager Meccanim FSMs.
    Adding the Prefabs of all Cameras for the scene.
    Prepping the Button for the Main Menu Screen

---
+++ Assets/GameMan.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMan : MonoBehaviour {
+
+    public static GameMan GameInstance;
+    public Transform[] CarTransSpawn = new Transform[7];
+    // Use this for initialization
+	void Awake ()
+    {
+        GameInstance = this;
+    }
+    private void Start()
+    {
+        InitializeGame();
+    }
+
+    public static void Pause()
+    {
+
+    }
+    public static void InitializeAttractScreen()
+    {
+        CameraMan.CamInstance.CurrentCamerasPar = 0;
+    }
+    public static void InitializeGame()
+    {
+        CameraMan.CamInstance.CurrentCamerasPar = 1;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
